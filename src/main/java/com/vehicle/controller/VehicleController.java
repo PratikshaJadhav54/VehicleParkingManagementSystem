83c695e6f41aa0911d65189c7d8de3e1d72794ef
Pratiksha-Prakash-Jadhav <pratikshajadhav0372@gmail.com>
2025-12-30 14:28:10 +0530

    API to Create the Vehicle with Resident_id Implemented

---
+++ src/main/java/com/vehicle/controller/VehicleController.java
@@ -0,0 +1,24 @@
+package com.vehicle.controller;
+
+import com.vehicle.entity.Vehicle;
+import com.vehicle.service.VehicleService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/vehicle")
+public class VehicleController {
+
+    @Autowired
+    VehicleService vehicleService;
+
+    @PostMapping("/save/{resident_id}")
+    public ResponseEntity<Vehicle> saveVehicle(@Valid @RequestBody Vehicle vehicle, @PathVariable("resident_id") int resident_id)
+    {
+        Vehicle savedVehicle=vehicleService.save(vehicle,resident_id);
+        return new ResponseEntity<>(savedVehicle, HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/vehicle/service/VehicleService.java
@@ -0,0 +1,8 @@
+package com.vehicle.service;
+
+import com.vehicle.entity.Vehicle;
+
+public interface VehicleService {
+
+    Vehicle save(Vehicle vehicle,int resident_id);
+}
